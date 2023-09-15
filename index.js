525efd9a384697a61d4e857fa89e6f67c4e6d26b
ChetanBargali <chetanbargali022@gmail.com>
2023-09-16 00:15:17 +0530

    express install done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const app=express();
+const port=7000;
+
+
+
+
+
+
+
+
+//make the app listen
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is running on the port: ${port}`);
+});
