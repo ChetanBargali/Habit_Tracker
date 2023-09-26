034c3fccfb53eb4bbdb557ecaa83a552915a3122
Chetan Bargali <140116977+ChetanBargali@users.noreply.github.com>
2023-09-26 12:50:31 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 //connect database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Habit_Tracker');
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/Habit_Tracker');
 
 //acquire the connection(to check if it is sucessful)
 const db=mongoose.connection;
